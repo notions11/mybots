a5db1dc31c1cbab431c14945942e730f2ddc8dec
Noah Shen <noahjasonshen@gmail.com>
2023-01-06 18:35:03 -0600

    sensors

---
+++ analyze.py
@@ -0,0 +1,10 @@
+import numpy
+import matplotlib.pyplot
+
+backLegSensorValues = numpy.load("data/backLegSensorValues.npy")
+frontLegSensorValues = numpy.load("data/frontLegSensorValues.npy")
+
+matplotlib.pyplot.plot(backLegSensorValues, label="backLeg", linewidth=3)
+matplotlib.pyplot.plot(frontLegSensorValues, label="frontLeg")
+matplotlib.pyplot.legend()
+matplotlib.pyplot.show()
--- simulate.py
+++ simulate.py
@@ -1,18 +1,29 @@
 import pybullet as p
 import time
 import pybullet_data
+import pyrosim.pyrosim as pyrosim
+import numpy
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
+p.setGravity(0,0,-92.8)
 planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
+pyrosim.Prepare_To_Simulate(robotId)
+backLegSensorValues = numpy.zeros(1000)
+frontLegSensorValues = numpy.zeros(1000)
+
 for i in range(1000):
     p.stepSimulation()
-    print(i)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)
 
 
 p.disconnect()
+
+print(backLegSensorValues)
+numpy.save("data/backLegSensorValues", backLegSensorValues)
+numpy.save("data/frontLegSensorValues", frontLegSensorValues)
