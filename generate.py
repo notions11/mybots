74302de68f839fa6630a8f4293141fb2c9f11cae
Noah Shen <noahjasonshen@gmail.com>
2023-01-06 18:05:43 -0600

    joints

--- generate.py
+++ generate.py
@@ -1,13 +1,19 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    pyrosim.Send_Cube(name="Box", pos=[0,2,0.5], size=[1,1,1])
+    pyrosim.End()
 
-#pyrosim.Send_Cube(name="Box", pos=[0,0,0.5], size=[1,1,1])
-#pyrosim.Send_Cube(name="Box", pos=[1,0,1.5], size=[1,1,1])
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 1.5], size=[1, 1, 1])
+    pyrosim.Send_Joint(name="Torso_BackLeg", parent="Torso", child="BackLeg", type="revolute", position=[0.5, 0, 1])
+    pyrosim.Send_Cube(name="BackLeg", pos=[0.5, 0, -0.5], size=[1, 1, 1])
 
-for i in range(-2, 3):
-    for j in range(-2, 3):
-        for k in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[i, j, 0.5+(k)], size=[1*(.9**k), 1*(.9**k), 1*(.9**k)])
+    pyrosim.Send_Joint(name="Torso_FrontLeg", parent="Torso", child="FrontLeg", type="revolute", position=[-0.5, 0, 1])
+    pyrosim.Send_Cube(name="FrontLeg", pos=[-0.5, 0, -0.5], size=[1, 1, 1])
+    pyrosim.End()
 
-pyrosim.End()
+Create_World()
+Create_Robot()
