1a78a6b7502f306584def15c03cd10a9bd544dd8
Noah Shen <noahjasonshen@gmail.com>
2023-01-19 20:03:50 -0600

    neurons

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -28,6 +28,26 @@ class NEURAL_NETWORK:
 
         print("")
 
+    def Update(self):
+        for neuronName in self.neurons.keys():
+            if self.neurons[neuronName].Is_Sensor_Neuron():
+                self.neurons[neuronName].Update_Sensor_Neuron()
+            else:
+                self.neurons[neuronName].Update_Hidden_Or_Motor_Neuron()
+
+    def Get_Neuron_Names(self):
+        return self.neurons.keys()
+
+    def Is_Motor_Neuron(self, n):
+        return self.neurons[n].Is_Motor_Neuron()
+
+    def Get_Motor_Neurons_Joint(self, n):
+        return self.neurons[n].Get_Joint_Name()
+
+    def Get_Value_Of(self, n):
+        return self.neurons[n].Get_Value()
+
+
 # ---------------- Private methods --------------------------------------
 
     def Add_Neuron_According_To(self,line):
--- robot.py
+++ robot.py
@@ -5,11 +5,13 @@ import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy
 import constants as c
+from pyrosim.neuralNetwork import NEURAL_NETWORK
 
 class ROBOT:
     def __init__(self):
         self.motors = dict()
         self.robotId = p.loadURDF("body.urdf")
+        self.nn = NEURAL_NETWORK("brain.nndf")
         pyrosim.Prepare_To_Simulate(self.robotId)
 
     def Prepare_To_Sense(self):
@@ -27,5 +29,15 @@ class ROBOT:
             self.motors[jointName] = MOTOR(jointName)
 
     def Act(self, t):
-        for i in self.motors:
-            self.motors[i].Set_Value(self.robotId, t)
+        for neuronName in self.nn.Get_Neuron_Names():
+            if self.nn.Is_Motor_Neuron(neuronName):
+                jointName = bytes(self.nn.Get_Motor_Neurons_Joint(neuronName), encoding='utf8')
+                desiredAngle = self.nn.Get_Value_Of(neuronName)
+                self.motors[jointName].Set_Value(self.robotId, desiredAngle)
+                print(neuronName, jointName, desiredAngle)
+        #for i in self.motors:
+        #    self.motors[i].Set_Value(self.robotId, desiredAngle)
+
+    def Think(self):
+        self.nn.Update()
+        self.nn.Print()
